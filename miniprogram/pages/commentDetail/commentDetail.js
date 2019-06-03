2b202ed2bf87303bb8b2eaabc3f576b2ad92565e
taffy_tian <452339112@qq.com>
2019-06-03 10:55:01 +0800

    影评详情页编写

--- miniprogram/pages/commentDetail/commentDetail.js
+++ miniprogram/pages/commentDetail/commentDetail.js
@@ -5,11 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-    comment:{
-      
-    }
+    comment: {
+      image: 'https://7468-thinking-bcedc9-1257745530.tcb.qcloud.la/p2517753454.jpg?sign=62e0d38d24597b557a04b400ba4e7cea&t=1559121186',
+      title:'复仇者联盟3：无限战争',
+      headshort: '../../images/juide.jpg',
+      name: '小变态',
+      content: '我是文字影评我是文字影评我是文字影评我是文字影评我是文字影评我是文字影评我是文字影评我是文字影评我是文字影评我是文字影评'
+    },
+    actionSheetHidden: true,
+    actionSheetItems: ['文字', '音频']
   },
-
+  // 底部弹出框
+  actionSheetTap() {
+    this.setData({
+      actionSheetHidden: !this.data.actionSheetHidden
+    })
+  },
+  actionSheetChange() {
+    this.setData({
+      actionSheetHidden: !this.data.actionSheetHidden
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
