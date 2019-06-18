86e683d20f37f6b14c2e675aa84aef76e8803f4c
taffy_tian <452339112@qq.com>
2019-06-18 17:01:42 +0800

    添加影评

--- miniprogram/pages/previewComment/previewComment.js
+++ miniprogram/pages/previewComment/previewComment.js
@@ -1,19 +1,34 @@
 // miniprogram/pages/previewComment/previewComment.js
+const db = wx.cloud.database();
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-  image: '',
-  title: '',
-  headshort: '',
-  name: '',
-  content: ''
+    image: '',
+    title: '',
+    headshort: '',
+    name: '',
+    content: ''
   },
-  backToComment(){
+  backToComment() {
     wx.navigateBack()
   },
-  saveToComment(){
+  saveToComment() {
+    db.collection('movieComments').add({
+      data: {
+        name: this.data.name,
+        title: this.data.title,
+        headshort: this.data.headshort,
+        name: this.data.name,
+        content: this.data.content,
+        image:this.data.image
+      },
+      success:(res)=>{
+        console.log(res)
+      },
+      fail:console.error
+    }) 
     wx.navigateTo({
       url: '../commentList/commentList',
     })
@@ -25,8 +40,7 @@ Page({
     let that = this
     let movieDetail = wx.getStorageSync('movieDetail')
     wx.getUserInfo({
-      success: function(res) {
-        console.log(res.userInfo)
+      success: function (res) {
         that.setData({
           headshort: res.userInfo.avatarUrl,
           name: res.userInfo.nickName,
