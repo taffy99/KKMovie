bfa1d61e2ea63a46a84ad642d3e67f440aad992a
taffy_tian <452339112@qq.com>
2019-07-08 16:28:09 +0800

    推荐电影滚动效果

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -4,16 +4,22 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: { 
-    recommendMovie:{}, // 推荐电影
-    movieTitle:'',
-    movieImg:'' ,
-    headshort:'',
-    name:''
-    },
-    
+  data: {
+    recommendMovie: {}, // 推荐电影
+    recommendList: [], // 推荐电影列表
+    movieTitle: '',
+    movieImg: '',
+    headshort: '',
+    name: '',
+    indicatorDots: false,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    circular: true
+  },
+
   // 跳转热门电影
-  skipToHot(){
+  skipToHot() {
     wx.navigateTo({
       url: '../hotMovie/hotMovie',
     })
@@ -26,13 +32,13 @@ Page({
   },
   // 跳转影评详情
   skipToComment() {
-    let commentId  = this.data.recommendMovie._id
+    let commentId = this.data.recommendMovie._id
     wx.navigateTo({
       url: '../commentDetail/commentDetail?commentId=' + commentId,
     })
   },
   //跳转电影详情
-  skipToDetail(){
+  skipToDetail() {
     let movieId = this.data.recommendMovie.movieId
     wx.navigateTo({
       url: '../movieDetail/movieDetail?movieId=' + movieId,
@@ -41,38 +47,41 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     wx.cloud.callFunction({
       name: 'movieComments',
       success: res => {
         let recommendMovie = this.getRandomMovie(res.result.data);
+        let recommendList = res.result.data
         this.setData({
           recommendMovie,
+          recommendList,
           movieTitle: recommendMovie.title,
           movieImg: recommendMovie.image,
           headshort: recommendMovie.headshort,
           name: recommendMovie.name
         })
+        console.log(recommendList)
       },
-      fail:console.errorerror
+      fail: console.errorerror
     })
   },
   //获取随机电影
-  getRandomMovie(movieComments){
+  getRandomMovie(movieComments) {
     let movie = movieComments[Math.floor(Math.random() * movieComments.length)]
     return movie
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-     
+  onReady: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   }
 })
